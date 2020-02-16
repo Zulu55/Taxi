be39aed620da1768302a016b419d5a4eefbd0672
Juan Carlos Zuluaga Cardona <jzuluaga55@hotmail.com>
2020-02-16 18:02:32 -0500

    Show taxi history

---
+++ Taxi.Common/Services/ApiService.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Taxi.Common.Models;
+
+namespace Taxi.Common.Services
+{
+    public class ApiService : IApiService
+    {
+        public async Task<Response> GetTaxiAsync(string plaque, string urlBase, string servicePrefix, string controller)
+        {
+            try
+            {
+                HttpClient client = new HttpClient
+                {
+                    BaseAddress = new Uri(urlBase),
+                };
+
+                string url = $"{servicePrefix}{controller}/{plaque}";
+                HttpResponseMessage response = await client.GetAsync(url);
+                string result = await response.Content.ReadAsStringAsync();
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    return new Response
+                    {
+                        IsSuccess = false,
+                        Message = result,
+                    };
+                }
+
+                TaxiResponse model = JsonConvert.DeserializeObject<TaxiResponse>(result);
+                return new Response
+                {
+                    IsSuccess = true,
+                    Result = model
+                };
+            }
+            catch (Exception ex)
+            {
+                return new Response
+                {
+                    IsSuccess = false,
+                    Message = ex.Message
+                };
+            }
+        }
+    }
+}
--- Taxi.Prism/Taxi.Prism/App.xaml.cs
+++ Taxi.Prism/Taxi.Prism/App.xaml.cs
@@ -25,6 +25,7 @@ namespace Taxi.Prism
         protected override void RegisterTypes(IContainerRegistry containerRegistry)
         {
             containerRegistry.Register<IGeolocatorService, GeolocatorService>();
+            containerRegistry.Register<IApiService, ApiService>();
             containerRegistry.RegisterForNavigation<NavigationPage>();
             containerRegistry.RegisterForNavigation<HomePage, HomePageViewModel>();
             containerRegistry.RegisterForNavigation<TaxiMasterDetailPage, TaxiMasterDetailPageViewModel>();
