db2c8aeaf9964319e77a53697846825052872942
Juan Carlos Zuluaga Cardona <jzuluaga55@hotmail.com>
2020-02-18 17:14:05 -0500

    Change password

--- Taxi.Web/Controllers/AccountController.cs
+++ Taxi.Web/Controllers/AccountController.cs
@@ -162,5 +162,37 @@ namespace Taxi.Web.Controllers
 
             return View(model);
         }
+
+        public IActionResult ChangePassword()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ChangePassword(ChangePasswordViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userHelper.GetUserByEmailAsync(User.Identity.Name);
+                if (user != null)
+                {
+                    var result = await _userHelper.ChangePasswordAsync(user, model.OldPassword, model.NewPassword);
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("ChangeUser");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, result.Errors.FirstOrDefault().Description);
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "User no found.");
+                }
+            }
+
+            return View(model);
+        }
     }
 }
