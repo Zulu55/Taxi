8843865c6007ab282932f3d45399c5d79c4061a1
Juan Carlos Zuluaga Cardona <jzuluaga55@hotmail.com>
2020-03-17 16:19:50 -0500

    PNPOC

--- Taxi.Prism/Taxi.Prism.Android/MainActivity.cs
+++ Taxi.Prism/Taxi.Prism.Android/MainActivity.cs
@@ -1,12 +1,16 @@
 ﻿using Android.App;
 using Android.Content.PM;
+using Android.Gms.Common;
 using Android.OS;
+using Android.Util;
 using Plugin.CurrentActivity;
 using Plugin.Permissions;
 using Prism;
 using Prism.Ioc;
 using Syncfusion.SfBusyIndicator.XForms.Droid;
 using Syncfusion.SfRating.XForms.Droid;
+using System;
+using Taxi.Common.Constants;
 
 namespace Taxi.Prism.Droid
 {
@@ -30,6 +34,49 @@ namespace Taxi.Prism.Droid
             new SfBusyIndicatorRenderer();
             new SfRatingRenderer();
             LoadApplication(new App(new AndroidInitializer()));
+
+            if (IsPlayServiceAvailable() == false)
+            {
+                throw new Exception("This device does not have Google Play Services and cannot receive push notifications.");
+            }
+
+            CreateNotificationChannel();
+        }
+
+        private bool IsPlayServiceAvailable()
+        {
+            int resultCode = GoogleApiAvailability.Instance.IsGooglePlayServicesAvailable(this);
+            if (resultCode != ConnectionResult.Success)
+            {
+                if (GoogleApiAvailability.Instance.IsUserResolvableError(resultCode))
+                {
+                    Log.Debug(AppConstants.DebugTag, GoogleApiAvailability.Instance.GetErrorString(resultCode));
+                }
+                else
+                {
+                    Log.Debug(AppConstants.DebugTag, "This device is not supported");
+                }
+                return false;
+            }
+            return true;
+        }
+
+        private void CreateNotificationChannel()
+        {
+            // Notification channels are new as of "Oreo".
+            // There is no need to create a notification channel on older versions of Android.
+            if (Build.VERSION.SdkInt >= BuildVersionCodes.O)
+            {
+                string channelName = AppConstants.NotificationChannelName;
+                string channelDescription = string.Empty;
+                NotificationChannel channel = new NotificationChannel(channelName, channelName, NotificationImportance.Default)
+                {
+                    Description = channelDescription
+                };
+
+                NotificationManager notificationManager = (NotificationManager)GetSystemService(NotificationService);
+                notificationManager.CreateNotificationChannel(channel);
+            }
         }
 
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Permission[] grantResults)
@@ -47,4 +94,3 @@ namespace Taxi.Prism.Droid
         }
     }
 }
-
--- Taxi.Prism/Taxi.Prism/ViewModels/HomePageViewModel.cs
+++ Taxi.Prism/Taxi.Prism/ViewModels/HomePageViewModel.cs
@@ -1,13 +1,31 @@
 ﻿using Prism.Navigation;
+using Taxi.Prism.Helpers;
+using Xamarin.Forms;
 
 namespace Taxi.Prism.ViewModels
 {
     public class HomePageViewModel : ViewModelBase
     {
+        private static HomePageViewModel _instance;
+
         public HomePageViewModel(INavigationService navigationService)
             : base(navigationService)
         {
+            _instance = this;
             Title = "Taxi Qualifier";
+        }
+
+        public static HomePageViewModel GetInstance()
+        {
+            return _instance;
+        }
+
+        public void AddMessage(string message)
+        {
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                App.Current.MainPage.DisplayAlert(Languages.Error, message, Languages.Accept);
+            });
         }
     }
 }
